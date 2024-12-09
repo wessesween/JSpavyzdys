328c4f90d9d4ba090037102364ed1a65ce192d9f
wessesween <sas.shved998@gmail.com>
2024-12-09 15:37:59 +0200

    trecias divas. + js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+//pirmas div
 document.getElementById('pirma-antraste').innerHTML = 'Naujas pirmos antrastes tekstas';
-document.getElementById('pirmas-paragrafas').innerHTML = 'Naujas pakeistas pirmo paragrafo tekstas'
+document.getElementById('pirmas-paragrafas').innerHTML = 'Naujas pakeistas pirmo paragrafo tekstas';
+
+//antras div
+
+console.log('ANTRAS DIV');
+
+let tekstasIsParagrafo = document.getElementById('paragrafo-tekstas').innerHTML;
+console.log('paimtas su innerHTML tekstas:',tekstasIsParagrafo );
+
+let tekstasIsParagrafo2 = document.getElementById('paragrafo-tekstas').innerHTML;
+console.log('paimtas su innerHTML tekstas:',tekstasIsParagrafo2 );
+
+console.log();
