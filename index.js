0f60bc419eeb1b1593167b7207184b82b3bc7717
wessesween <sas.shved998@gmail.com>
2024-12-09 15:28:55 +0200

    js tekstas. istaysyta klaida css faile

---
+++ index.js
@@ -0,0 +1,2 @@
+document.getElementById('pirma-antraste').innerHTML = 'Naujas pirmos antrastes tekstas';
+document.getElementById('pirmas-paragrafas').innerHTML = 'Naujas pakeistas pirmo paragrafo tekstas'
