c3d3fd07e64bff4bcb855b9af6321fb8c27dbd4c
wessesween <sas.shved998@gmail.com>
2024-12-10 11:14:41 +0200

    nuo 8 iki10 ir 13 uzd

--- index.js
+++ index.js
@@ -30,4 +30,58 @@ function pakeisti(){
 //astuntas div
 function pakeisti2(elementas, tekstas){
     document.getElementById(elementas).innerHTML = tekstas;
-}
+};
+
+//devintas div
+
+function pakeisti3(elementas, tekstas){
+    document.getElementById(elementas).innerHTML=tekstas;
+};
+
+function kitaSpalva(elementas,spalva){
+    document.getElementById(elementas).style.color = spalva;
+};
+
+//desimtas div
+
+var pirmas = 8;
+var antras = 9;
+
+var suma = pirmas + antras;
+var skirtumas = pirmas - antras;
+var sandauga = pirmas * antras;
+var dalmuo = pirmas / antras;
+
+document.getElementById("suma").innerHTML = pirmas+ "+" + antras + "=" + suma;
+document.getElementById("skirtumas").innerHTML = pirmas+ "-" + antras + "=" + skirtumas;
+document.getElementById("sandauga").innerHTML = pirmas+ "*" + antras + "=" + sandauga;
+document.getElementById("dalmuo").innerHTML = pirmas+ "/" + antras + "=" + dalmuo;
+
+
+
+//tryliktas div
+
+let divKeitimui = document.getElementById('div-keitimui');
+
+let plocioSlider = document.getElementById("plocio-slider");
+let aukscioSlider = document.getElementById("aukscio-slider");
+
+plocioSlider.oninput = function(){
+    divKeitimui.style.width = this.value + 'px';
+};
+
+aukscioSlider.oninput = function(){
+    divKeitimui.style.height = this.value + 'px';
+};
+
+let spalvosPasirinkimas = document.getElementById('spalvos-pasirinkimas');
+
+spalvosPasirinkimas.oninput = function(){
+    divKeitimui.style.backgroundColor = this.value;
+};
+
+let tekstasDivui = document.getElementById('tekstas-divui');
+
+tekstasDivui.oninput = function(){
+    divKeitimui.innerHTML = this.value;
+};
